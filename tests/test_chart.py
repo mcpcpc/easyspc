420ba2e494d6259a7d006c1486987a0d1fa38d42
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2024-09-08 04:51:40 -0400

    update

--- tests/test_chart.py
+++ tests/test_chart.py
@@ -5,36 +5,52 @@ from unittest import TestCase
 from unittest import main
 from unittest.mock import patch
 
+from easyspc.chart import batched
 from easyspc.chart import load_template
-from easyspc.chart import imr
-from easyspc.chart import xbarr
-from easyspc.chart import xbars
+from easyspc.chart import IMR
+from easyspc.chart import XBarR
+from easyspc.chart import XBarS
 
 
 class ChartTestCase(TestCase):
+    def test_batched(self):
+        iterable = list(range(6))
+        response = batched(iterable, n=2)
+        self.assertEqual(len(response), 3)
+
+    def test_batched_error(self):
+        iterable = list(range(6))
+        with self.assertRaises(ValueError):
+            batched(iterable, n=0)
+
     @patch("easyspc.chart.get_data")
     def test_load_template(self, get_data_mock):
         get_data_mock.return_value = b"{}"
         load_template("test")
         get_data_mock.assert_called_once()
 
-    def test_imr(self):
-        x = [1, 2, 3]
-        response = imr(x=x)
+    @patch("easyspc.chart.get_data")
+    def test_load_template_error(self, get_data_mock):
+        get_data_mock.return_value = None
+        with self.assertRaises(Exception):
+            load_template("test")
+
+    def test_i_mr(self):
+        data = list(range(3))
+        imr = IMR(data=data)
+        response = imr.plot()
         self.assertIsInstance(response, dict)
 
-    def test_xbarr(self):
-        x = [
-            list(range(3)),
-        ] * 3
-        response = xbarr(x=x)
+    def test_x_bar_r(self):
+        x = list(range(9))
+        xbarr = XBarR(data=data, subgroup_size=3)
+        response = xbarr.plot()
         self.assertIsInstance(response, dict)
 
-    def test_xbars(self):
-        x = [
-            list(range(9)),
-        ] * 3
-        response = xbars(x=x)
+    def test_x_bar_s(self):
+        x = list(range(9))
+        xbars = XBarS(data=data, subgroup_size=3)
+        response = xbars.plot()
         self.assertIsInstance(response, dict)
 
 
