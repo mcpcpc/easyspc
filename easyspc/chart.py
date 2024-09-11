b2342a7437ddab85900fb82ec4a8c8352bb7ef2c
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2024-09-10 20:27:29 -0400

    update

--- easyspc/chart.py
+++ easyspc/chart.py
@@ -376,7 +376,7 @@ class P:
     @property
     def lower_control_limit(self) -> list | float:
         p_bar = self.center_line
-        if isinstance(p_bar, list):
+        if isinstance(self.sample_sizes, list):
             return p_bar - 3 * (
                 p_bar * (1 - p_bar)
             ) ** (.5) / (self.sample_sizes ** (.5))
